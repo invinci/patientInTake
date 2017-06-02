dcfe8c5fdd727ff91ca6024a9dd77686f6da43cb
Anuj Sharma <anujsh@smartdatainc.net>
2017-06-02 15:09:42 +0530

    db changes

--- db.js
+++ db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    'url' : 'mongodb://PatientIntakeWeb:PatientIntakeWeb2780@localhost:27017/PatientIntakeWeb' // looks like mongodb://<user>:<pass>@mongo.onmodulus.n$
+    'url' : 'mongodb://PatientIntakeWeb:PatientIntakeWeb2780@52.39.212.226:27017/PatientIntakeWeb' // looks like mongodb://<user>:<pass>@mongo.onmodulus.n$
 };
